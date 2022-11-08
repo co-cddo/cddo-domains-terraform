eb9a4d85d88218be985423e6b992d56a453c5400
OllieJC <5426038+OllieJC@users.noreply.github.com>
2022-11-08 18:51:08 +0000

    Add security_policy

--- dns/route53-cddo-co-gov-uk.tf
+++ dns/route53-cddo-co-gov-uk.tf
@@ -48,9 +48,12 @@ resource "aws_route53_record" "www-aaaa" {
 }
 
 module "co-cddo-aws-r53-parked-domain" {
-  source            = "github.com/co-cddo/aws-route53-parked-govuk-domain//terraform?ref=829478ba8ed41863d7e5f526475de3e09171da4d"
-  zone_id           = aws_route53_zone.cddo-cabinetoffice-gov-uk.zone_id
-  depends_on        = [aws_route53_zone.cddo-cabinetoffice-gov-uk]
-  email_records     = true  # default
-  webserver_records = false # default
+  source                 = "github.com/co-cddo/aws-route53-parked-govuk-domain//terraform?ref=5e85556ce417cd335c440fd1e7079bd331f443d5"
+  zone_id                = aws_route53_zone.cddo-cabinetoffice-gov-uk.zone_id
+  depends_on             = [aws_route53_zone.cddo-cabinetoffice-gov-uk]
+  email_records          = true  # default
+  webserver_records      = false # default
+  additional_txt_records = [
+    "security_policy=https://vulnerability-reporting.service.security.gov.uk/.well-known/security.txt"
+  ]
 }
